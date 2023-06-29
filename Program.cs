6b1c8f32a71909792815305a1dac126df75fdf08
emandii <130602804+emandii@users.noreply.github.com>
2023-06-29 18:37:39 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//OBFUSCATE EMAIL
+
+string email = "awesome@dotnet.com";
+string obfuscatedEmail = ObfuscateEmail(email);
+Console.WriteLine(obfuscatedEmail);
+
+static string ObfuscateEmail(string email)
+{
+    int atIndex = email.IndexOf('@');
+    string username = email.Substring(0, atIndex);
+    string domain = email.Substring(atIndex);
+
+    string obfuscatedUsername = new string('*', username.Length);
+    string obfuscatedEmail = obfuscatedUsername + domain;
+
+    return obfuscatedEmail;
+}
+
+Console.ReadKey();
